73d87c481b9f7fc8c0d3c29dbd147382f99ca817
MojaCZ <lukas.posekany@gmail.com>
2018-04-26 15:18:44 +0200

    histogram comparison

--- iris-recognition/iris.cpp
+++ iris-recognition/iris.cpp
@@ -2,45 +2,38 @@
 #include <iostream>
 #include <string>
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 #include <dirent.h>
 #include <unistd.h>
 #include <sys/types.h>
 
 int compare(cv::Mat, cv::Mat, int);
+std::vector<std::string> loadDir(std::string);
+int train(std::vector<std::string>);
+int getMax(cv::Mat, cv::Mat, int (&min)[3], int);
+int compareHistograms(cv::Mat, cv::Mat);
 
 int main(int argc, char const *argv[]) {
 
-  // get current working directory and append relative path to files
-  char cwd[1024];
-  getcwd(cwd, sizeof(cwd)) != NULL;
-  std::string imagesDir(cwd);
-  imagesDir.append("/irises_MICHE_iPhone5_norm/");
+  std::vector<std::string> paths;
+  paths = loadDir("/irises_MICHE_iPhone5_norm/");
 
-  // get all fileNames in directory
-  std::vector<std::string> v; // vector of files in directory
-  DIR* dirp = opendir(strdup(imagesDir.c_str()));
-  struct dirent * dp;
-  while ((dp = readdir(dirp)) != NULL) {
-    v.push_back(dp->d_name);
-  //   // std::cout << dp->d_name << std::endl;
-  }
-  closedir(dirp);
 
-  for (int i=2; i<v.size(); i++) {
-    std::cout << v[i] << std::endl;
-  }
+  // train(paths);
 
-  // for (std::vector<char>::const_iterator i = v.begin(); i != v.end();i++) {
-  //   std::cout << *i << std::endl;
-  // }
-  // for (auto i = v.begin(); i != v.end()-1; i++) {
+
+  // for (int i=2; i<paths.size(); i++) {
+  //   // std::cout << v[i] << std::endl;
+  //   // std::cout << v[i].substr(0,v[i].find("/",0)) << std::endl;
+  //
+  //   std::string ID = paths[i].substr(0, paths[i].find("/",0));
+  //   if (ID.substr(0,ID.find("_",0)) == "001") {
+  //     std::cout << ID << std::endl;
+  //   }
+  //   // std::cout << ID.substr(0,ID.find("_",0)) << std::endl;
   // }
 
-
-
-
-
-  const int WINDOW = 30;
+  const int WINDOW = 50;
 
   cv::Mat image1;
   cv::Mat image2;
@@ -52,8 +45,8 @@ int main(int argc, char const *argv[]) {
     return -1;
   }
 
-  // image2 = cv::imread("irises_MICHE_iPhone5_norm/004_IP5_OU_F_LI_01_1.iris.norm.png");
-  image2 = cv::imread("irises_MICHE_iPhone5_norm/001_IP5_IN_F_RI_01_2.iris.norm.png");
+  image2 = cv::imread("irises_MICHE_iPhone5_norm/004_IP5_OU_F_LI_01_1.iris.norm.png");
+  // image2 = cv::imread("irises_MICHE_iPhone5_norm/001_IP5_IN_F_RI_01_2.iris.norm.png");
   if (! image2.data) {
     std::cout << "Could not open or find image2" << std::endl;
     return -1;
@@ -64,12 +57,9 @@ int main(int argc, char const *argv[]) {
     return -1;
   }
 
+  compareHistograms(image1, image2);
+  // for (int i=0; i<)
   // compare(image1, image2, WINDOW);
-
-  // set variables
-  std::cout << "window size: " << WINDOW << std::endl;
-  std::cout << "width image1: " << image1.cols << " width image2: " << image2.cols <<  std::endl;
-  std::cout << "height image1: " << image1.rows << " height image2: " << image2.rows << std::endl;
 
   // cv::namedWindow("Display window", cv::WINDOW_AUTOSIZE);
   // cv::imshow("image1", image1);
@@ -79,29 +69,138 @@ int main(int argc, char const *argv[]) {
   return 0;
 }
 
-int compare(cv::Mat image1, cv::Mat image2, int WINDOW) {
+std::vector<std::string> loadDir(std::string relativePath) {
+  // get current working directory and append relative path to files
+  char cwd[1024];
+  getcwd(cwd, sizeof(cwd)) != NULL;
+  std::string imagesDir(cwd);
+  imagesDir.append(relativePath);
 
-  std::cout << "from function" << std::endl;
-  std::cout << "width image1: " << image1.cols << " width image2: " << image2.cols <<  std::endl;
-  std::cout << "height image1: " << image1.rows << " height image2: " << image2.rows << std::endl;
+  // get all fileNames in directory
+  std::vector<std::string> paths; // vector of files in directory
+  DIR* dirp = opendir(strdup(imagesDir.c_str()));
+  struct dirent * dp;
+  int i = 0;
+  while ((dp = readdir(dirp)) != NULL) {
+    if (i>1) {
+      paths.push_back(imagesDir + (dp->d_name));
+    }
+    i++;
+  }
+  closedir(dirp);
+  return paths;
+}
 
-  for (int i=0; i<image1.cols-WINDOW; i=i+WINDOW) {
-    for (int j=0; j<image1.rows-WINDOW; j=j+WINDOW) {
-      int S1 = 0;
-      int S2 = 0;
-      for (int x=i; x<i+WINDOW; x++) {
-        for (int y=j; y<j+WINDOW; y++) {
-          cv::Vec3b px1 = image1.at<cv::Vec3b>(y, x);
-          cv::Vec3b px2 = image2.at<cv::Vec3b>(y, x);
-          S1 += px1[0];
-          S2 += px2[0];
+int compareHistograms(cv::Mat img1, cv::Mat img2) {
+  cv::Mat hsv_img1; cv::Mat hsv_img2;
+
+  cv::cvtColor( img1, hsv_img1, cv::COLOR_BGR2HSV);
+  cv::cvtColor( img2, hsv_img2, cv::COLOR_BGR2HSV);
+
+
+  int h_bins = 50; int s_bins = 60;
+  int histSize[] = { h_bins, s_bins };
+
+  float h_ranges[] = { 0, 180 };
+  float s_ranges[] = { 0, 256 };
+
+  const float* ranges[] = { h_ranges, s_ranges };
+
+  int channels[] = { 0, 1 };
+
+  cv::MatND hist_img1;
+  cv::MatND hist_img2;
+
+  cv::calcHist( &hsv_img1, 1, channels, cv::Mat(), hist_img1, 2, histSize, ranges, true, false);
+  cv::normalize( hist_img1, hist_img1, 0, 1, cv::NORM_MINMAX, -1, cv::Mat());
+
+  cv::calcHist( &hsv_img2, 1, channels, cv::Mat(), hist_img2, 2, histSize, ranges, true, false);
+  cv::normalize( hist_img2, hist_img2, 0, 1, cv::NORM_MINMAX, -1, cv::Mat());
+
+  // 0 = correlation, 1 = CHI2, 2 = intersection, 3 = BhattacharyyaD, 4 = synonym
+  double M1 = cv::compareHist( hist_img1, hist_img2, 0);
+  double M2 = cv::compareHist( hist_img1, hist_img2, 1);
+  double M3 = cv::compareHist( hist_img1, hist_img2, 2);
+  double M4 = cv::compareHist( hist_img1, hist_img2, 3);
+  double M5 = cv::compareHist( hist_img1, hist_img2, 4);
+
+  std::cout << "comparison:\t" << M1 << "\t" << M2 << "\t" << M3 << "\t" << M4 << "\t" << M5 << std::endl;
+}
+
+int train(std::vector<std::string> paths) {
+  // if I have 815 files in dir, and I want to compare each with each,
+  // C(815,2) = (815*814)/(2!) = 331705 of combinations
+
+  int min[3] = {255,255,255}, max[3] = {0, 0, 0};
+  // different window
+
+  // each with each
+  for (int i=0; i<paths.size(); i++) {  //paths.size()
+    for (int j=i+1; j<paths.size(); j++) {  //paths.size()
+      cv::Mat img1 = cv::imread(paths[i]);
+      cv::Mat img2 = cv::imread(paths[j]);
+      getMax(img1, img2, max, 20);
+      std::cout << "MAX IS: [" << max[0] << ", " << max[1] << ", " << max[2] << "]" << std::endl;
+    }
+  }
+    // find min max average
+}
+
+int getMax(cv::Mat img1, cv::Mat img2, int (&max)[3], int WINDOW) {
+  int windowSize = WINDOW * WINDOW;
+  for (int j=0; j<img1.cols-WINDOW; j++) {
+    for (int i=0; i<img1.rows-WINDOW; i++) {
+      int B1=0, G1=0, R1 = 0;
+      int B2=0, G2=0, R2 = 0;
+      for (int y=j; y<j+WINDOW-1; y++) {
+        for (int x=i; x<i+WINDOW-1; x++) {
+          cv::Vec3b px1 = img1.at<cv::Vec3b>(x, y);
+          cv::Vec3b px2 = img2.at<cv::Vec3b>(x, y);
+          B1 += px1[0]; G1 += px1[1]; R1 += px1[2];
+          B2 += px2[0]; G2 += px2[1]; R2 += px2[2];
         }
       }
-      std::cout << i << " " << j << " " << S1 << " " << S2 << std::endl;
-    }
-    // std::cout << i << std::endl;
-    // cv::Vec3b px = image1.at<cv::Vec3b>(5, i);
-    // std::cout << px << std::endl;
-  }
 
+      int B1A = B1/windowSize, G1A = G1/windowSize, R1A = R1/windowSize; // get averageas of image pixel in each color
+      int B2A = B2/windowSize, G2A = G2/windowSize, R2A = R2/windowSize;
+      int diffB = abs(B1A - B2A), diffG = abs(G1A - G2A), diffR = abs(B1A - B2A);
+      // std::cout << "diffR: " << diffR << " diffG: " << diffG << " diffB: " << std::endl;
+      if (diffB > max[0]) { max[0] = diffB;}
+      // if (diffR > max[0]) { max[0] = diffR; std::cout << "new max R " << diffR << std::endl;}
+
+      if (diffG > max[1]) { max[1] = diffG;}
+
+      if (diffR > max[2]) { max[2] = diffR;}
+    }
+  }
+}
+
+int abs(int a) {
+  if (a<0) {
+    return -a;
+  } else {
+    return a;
+  }
+}
+
+int compare(cv::Mat image1, cv::Mat image2, int WINDOW) {
+  int windowSize = WINDOW * WINDOW;
+  // make a window in size of WINDOW, width i
+  for (int j=0; j<image1.cols-WINDOW; j++) {
+    for (int i=0; i<image1.rows-WINDOW; i++) {
+      int R1=0, G1=0, B1 = 0;
+      int R2=0, G2=0, B2 = 0;
+      for (int y=j; y<j+WINDOW-1; y++) {
+        for (int x=i; x<i+WINDOW-1; x++) {
+          cv::Vec3b px1 = image1.at<cv::Vec3b>(x, y);
+          cv::Vec3b px2 = image2.at<cv::Vec3b>(x, y);
+          R1 += px1[0]; G1 += px1[1]; B1 += px1[2];
+          R2 += px2[0]; G2 += px2[1]; B2 += px2[2];
+        }
+      }
+      int R1A = R1/windowSize, G1A = G1/windowSize, B1A = B1/windowSize;
+      int R2A = R2/windowSize, G2A = G2/windowSize, B2A = B2/windowSize;
+      std::cout << "WINDOW: [" << i << " " << j << "] " << " averageas difference img1-img2 [R G B]: " << R1A-R2A << "\t" << G1A-G2A << "\t" << B1A-B2A << std::endl;
+    }
+  }
 };
